f0822ddc76e2bd69ad98bcf5593241bc5661fb92
MinhNamThan <thanminhnam17022001@gmail.com>
2022-07-08 11:11:36 +0700

    Chapter 10: Updating, showing, and deleting users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  include Pagy::Backend
 
   before_action :set_locale
 
@@ -12,5 +13,13 @@ class ApplicationController < ActionController::Base
 
   def default_url_options
     {locale: I18n.locale}
+  end
+
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = t ".loggin_please"
+    edirect_to login_path
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,17 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, except: %i(show new create)
+  before_action :find_user, except: %i(new create index)
+  before_action :admin_user, only: %i(destroy)
+  before_action :correct_user, only: %i(edit update)
+
+  def index
+    @pagy, @users = pagy User.latest_user
+  end
+
   def show
-    @user = User.find_by id: params[:id]
     return if @user
 
-    flash[:warning] = t(".not_found")
+    flash[:warning] = t ".not_found"
     redirect_to root_path
   end
 
@@ -22,9 +30,52 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @user.update user_params
+      flash[:success] = t ".update_flash"
+      redirect_to @user
+    else
+      flash[:danger] = t ".update_fail"
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t ".destroy_flash"
+    else
+      flash[:danger] = t ".destroy_fail"
+    end
+    redirect_to users_path
+  end
+
   private
 
   def user_params
     params.require(:user).permit(User::USER_ATTRS)
+  end
+
+  def correct_user
+    return if current_user?(@user)
+
+    flash[:danger] = t "users.before_action.not_correct"
+    redirect_to root_path
+  end
+
+  def admin_user
+    return if current_user.admin?
+
+    flash[:danger] = t "users.before_action.not_admin"
+    redirect_to root_path
+  end
+
+  def find_user
+    @user = User.find_by id: params[:id]
+    return if @user
+
+    flash[:danger] = t ".not_found_before"
+    redirect_to root_path
   end
 end
